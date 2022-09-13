fd37e707d48112502f54222bf81301d4b096d90b
taissadames <taissadames@Marlons-MacBook-Pro.local>
2022-09-13 16:58:51 +0930

    code updated

--- db/schema.sql
+++ db/schema.sql
@@ -3,14 +3,16 @@ CREATE DATABASE f1_app;
 CREATE TABLE cars(
     id SERIAL PRIMARY KEY,
     name TEXT,
-    image_url TEXT
+    image_url TEXT,
+    owner TEXT 
+    
 );
 
-INSERT INTO cars(name, image_url)
-VALUES('Mercedes', 'https://images.unsplash.com/photo-1532906619279-a4b7267faa66?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1032&q=80');
+INSERT INTO cars(name, image_url, owner)
+VALUES('Mercedes', 'https://images.unsplash.com/photo-1532906619279-a4b7267faa66?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1032&q=80', 'a');
 
-INSERT INTO cars(name, image_url)
-VALUES('Ferrari', 'https://images.unsplash.com/photo-1508175911810-a4817cd3d7b4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1171&q=80');
+INSERT INTO cars(name, image_url, owner)
+VALUES('Ferrari', 'https://images.unsplash.com/photo-1508175911810-a4817cd3d7b4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1171&q=80', 'c');
 
 CREATE TABLE users(
     id SERIAL PRIMARY KEY,
@@ -20,3 +22,4 @@ CREATE TABLE users(
 );
 
 ALTER TABLE users ADD COLUMN password_digest TEXT;
+
--- models/car.rb
+++ models/car.rb
@@ -1,17 +1,17 @@
-def all_cars
-    run_sql("SELECT * FROM cars ORDER BY id")
+def all_cars(current_user)
+    run_sql("SELECT * FROM cars WHERE owner = $1 ORDER BY id", [current_user])
 end
 
-def create_car(name, image_url)
-    run_sql("INSERT INTO cars(name, image_url) VALUES($1, $2)", [name, image_url])
+def create_car(name, image_url, current_user)
+    run_sql("INSERT INTO cars(name, image_url, owner) VALUES($1, $2, $3)", [name, image_url, current_user])
 end
 
-def get_car(id)
-    run_sql("SELECT * FROM cars WHERE id = $1", [id])[0]
+def get_car(id, current_user)
+    run_sql("SELECT * FROM cars WHERE id = $1 AND owner = $2", [id, current_user])[0]
 end
 
-def update_car(id, name, image_url)
-    run_sql("UPDATE cars SET name = $2, image_url = $3 WHERE id = $1", [id, name, image_url])
+def update_car(id, name, image_url, current_user)
+    run_sql("UPDATE cars SET name = $2, image_url = $3 WHERE id = $1 AND owner = $4", [id, name, image_url, current_user])
 end
 
 def delete_car(id)
