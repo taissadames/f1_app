9e90f267cc0c8b5e9eb11c369fc8bf208f15245f
taissadames <taissadames@Marlons-MacBook-Pro.local>
2022-09-13 21:06:27 +0930

    f1 app debugging

--- models/car.rb
+++ models/car.rb
@@ -1,17 +1,17 @@
-def all_cars(user_id)
-    run_sql("SELECT * FROM cars WHERE owner = $1 ORDER BY id", [user_id])
+def all_cars(owner)
+    run_sql("SELECT * FROM cars WHERE owner = $1 ORDER BY id", [owner])
 end
 
-def create_car(name, image_url, user_id)
-    run_sql("INSERT INTO cars(name, image_url, owner) VALUES($1, $2, $3)", [name, image_url, user_id])
+def create_car(name, image_url, owner)
+    run_sql("INSERT INTO cars(name, image_url, owner) VALUES($1, $2, $3)", [name, image_url, owner])
 end
 
-def get_car(id, user_id)
-    run_sql("SELECT * FROM cars WHERE id = $1 AND owner = $2", [id, user_id])[0]
+def get_car(id, owner)
+    run_sql("SELECT * FROM cars WHERE id = $1 AND owner = $2", [id, owner])[0]
 end
 
-def update_car(id, name, image_url, user_id)
-    run_sql("UPDATE cars SET name = $2, image_url = $3 WHERE id = $1 AND owner = $4", [id, name, image_url, user_id])
+def update_car(id, name, image_url, owner)
+    run_sql("UPDATE cars SET name = $2, image_url = $3 WHERE id = $1 AND owner = $4", [id, name, image_url, owner])
 end
 
 def delete_car(id)
